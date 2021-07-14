0cea752dbf2cb73145ddfb5673389d1d28a2f0ea
Jieun Han <namu1092@ajou.ac.kr>
2021-07-14 17:31:29 +0900

    create 1202, 1270

---
+++ WEEK_02_DATA_STRUCTURE/JieunHan17/1202.cpp
@@ -0,0 +1,63 @@
+#include <cstdio>
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <utility>
+#include <algorithm>
+
+using namespace std;
+
+int N, K, M, V, C;
+long total = 0;
+priority_queue<int> jewel_price;
+vector<int> bag;
+vector<pair<int, int> > jewel; //무게, 가격
+
+/* 계속해서 보석을 넣으면 다른 보석을 넣을 때 비워줘야 하는 게 아닐까? */
+bool compare(pair<int, int> a, pair<int, int> b)
+{
+    return a.first < b.first;
+}
+
+int main()
+{
+    scanf("%d %d", &N, &K);
+    for (int i = 0; i < N; i++)
+    {
+        cin >> M >> V;
+        jewel.push_back(make_pair(M, V));
+    }
+
+    for (int i = 0; i < K; i++)
+    {
+        cin >> C;
+        bag.push_back(C);
+    }
+
+    sort(jewel.begin(), jewel.end(), compare);
+    sort(bag.begin(), bag.end());
+
+    int idx = 0;
+    for (int i = 0; i < K; i++)
+    {
+        for (int jewel_idx = idx; jewel_idx < N; jewel_idx++)
+        {
+            if (jewel[jewel_idx].first > bag[i])
+            {
+                break;
+            }
+            jewel_price.push(jewel[jewel_idx].second);
+            idx++;
+        }
+
+        if (!jewel_price.empty())
+        {
+            total += jewel_price.top();
+            jewel_price.pop();
+        }
+    }
+
+    printf("%ld", total);
+
+    return 0;
+}
---
+++ WEEK_02_DATA_STRUCTURE/JieunHan17/1270.cpp
@@ -0,0 +1,76 @@
+#include <cstdio>
+#include <algorithm>
+#include <map>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++)
+    {
+        int Ti;
+        scanf("%d", &Ti);
+
+        long long soldiers[Ti];
+        for (int j = 0; j < Ti; j++)
+        {
+            scanf("%lld", &soldiers[j]);
+        }
+        sort(soldiers, soldiers + Ti);
+
+        int cnt = 1;
+        bool flag = false;
+        for (int j = 0; j < Ti - 1; j++)
+        {
+            if (soldiers[j] == soldiers[j + 1])
+            {
+                cnt++;
+            }
+            else
+            {
+                cnt = 1;
+            }
+            if (cnt > Ti / 2)
+            {
+                printf("%lld\n", soldiers[j]);
+                flag = true;
+                break;
+            }
+        }
+        if (!flag)
+        {
+            printf("SYJKGW\n");
+        }
+
+        // map<long long, int> soldiers;
+        // for (int j = 0; j < Ti; j++)
+        // {
+        //     long long num;
+        //     scanf("%lld", &num);
+        //     soldiers[num]++;
+        // }
+
+        // bool flag = false;
+        // map<long long, int>::iterator iter;
+        // for (iter = soldiers.begin(); iter != soldiers.end(); iter++)
+        // {
+        //     if ((*iter).second > Ti / 2)
+        //     {
+        //         flag = true;
+        //         break;
+        //     }
+        // }
+        // if (flag)
+        // {
+        //     printf("%lld\n", (*iter).first);
+        // }
+        // else
+        // {
+        //     printf("SYJKGW\n");
+        // }
+    }
+    return 0;
+}
