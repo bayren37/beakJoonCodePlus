f47af7922ed8b91539793822d930cfab864d1ce0
suengwone <52214766+suengwone@users.noreply.github.com>
2021-07-15 18:29:40 +0900

    Create 2075.cpp

---
+++ WEEK_02_DATA_STRUCTURE/SW/2075.cpp
@@ -0,0 +1,31 @@
+#include <cstdio>
+#include <stack>
+#define SIZE 1501
+#define MIN -1000000000
+
+using namespace std;
+
+int main(void) {
+	int N, x, cnt = 0, index, Max;
+	scanf("%d", &N);
+	stack<int> arr[SIZE];
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			scanf("%d", &x);
+			arr[j].push(x);
+		}
+	}
+
+	while (cnt != N) {
+		Max = MIN;
+		for (int i = 0; i < N; i++) {
+			if (Max < arr[i].top()) {
+				index = i;
+				Max = arr[i].top();
+			}
+		}
+		arr[index].pop();
+		cnt += 1;
+	}
+	printf("%d\n", Max);
+}
