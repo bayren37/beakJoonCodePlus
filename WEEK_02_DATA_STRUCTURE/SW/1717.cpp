aff9270035f03b738ac24d2dc2652d4a000cd1fe
suengwone <52214766+suengwone@users.noreply.github.com>
2021-07-15 18:33:08 +0900

    Create 1717.cpp

---
+++ WEEK_02_DATA_STRUCTURE/SW/1717.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int parent[1000001];
+
+int find(int x) {
+	if (x == parent[x]) {
+		return x;
+	}
+	else {
+		return parent[x] = find(parent[x]);
+	}
+}
+
+void Union(int x, int y) {
+	x = find(x);
+	y = find(y);
+	if (x > y) {
+		parent[y] = x;
+	}
+	else {
+		parent[x] = y;
+	}
+}
+
+bool isSameParent(int x, int y) {
+	x = find(x);
+	y = find(y);
+	if (x == y) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+	int n, m;
+	cin >> n >> m;
+	int b, c;
+	bool a;
+	for (int i = 1; i <= n; i++) {
+		parent[i] = i;
+	}
+	for (int i = 0; i < m; i++) {
+		cin >> a >> b >> c;
+		if (a) {
+			isSameParent(b, c) ? cout << "YES" << '\n' : cout << "NO" << '\n';
+		}
+		else{
+			Union(b, c);
+		}
+	}
+
+	return 0;
+}
