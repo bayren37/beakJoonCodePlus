49c96a0a5f9f144fd9dfe6e0647e9a1d978e7b8b
suengwone <52214766+suengwone@users.noreply.github.com>
2021-09-08 22:57:21 +0900

    Update 2021_new_id.cpp

--- WEEK_10_KAKAO_CODING_TEST/LSW/2021_new_id.cpp
+++ WEEK_10_KAKAO_CODING_TEST/LSW/2021_new_id.cpp
@@ -1,48 +1,9 @@
 // 신규 아이디 추천
 
-#include <cstring>
+#include <string>
 #include <iostream>
 
 using namespace std;
-
-
-string step_1(string answer) {
-	string tmp="";
-	for (int i = 0; i < answer.size(); i++) {
-		if (answer[i] == '-' || answer[i] == '_' || answer[i] == '.' || isalnum(answer[i])) {
-			if (isupper(answer[i]))
-				tmp += tolower(answer[i]);
-			else
-				tmp += answer[i];
-		}
-	}
-	answer = tmp;
-	return answer;
-}
-
-string step_2(string answer) {
-	string tmp = "";
-	for (int i = 0; i < answer.size()-1; i++) {
-		if (!(answer[i] == '.' && answer[i + 1] == '.'))
-			tmp += answer[i];
-	}
-	if (*answer.rbegin() != '.') tmp += *answer.rbegin();
-
-	answer = tmp;
-	return answer;
-}
-
-string step_3(string answer) {
-	string tmp = "";
-	if (answer.empty()) return tmp;
-	answer[0] == '.' ?
-		tmp += answer.substr(1, answer.size() - 2) :
-		tmp += answer.substr(0, answer.size() - 1);
-	if (*answer.rbegin() != '.') tmp += *answer.rbegin();
-
-	answer = tmp;
-	return answer;
-}
 
 string step_4(string answer) {
 	if (answer.size() >= 16) {
@@ -51,7 +12,7 @@ string step_4(string answer) {
 			answer.resize(14);
 	}
 	else if (answer.size() <= 2) {
-		if (answer.empty()) answer = 'a';
+		if (answer=="") answer = "a";
 		char tmp = *answer.rbegin();
 		while (answer.size() != 3) {
 			answer += tmp;
@@ -60,6 +21,41 @@ string step_4(string answer) {
 	return answer;
 }
 
+string step_3(string answer) {
+	string tmp = "";
+	if (answer.empty()) return step_4(tmp);
+	answer[0] == '.' ?
+		tmp += answer.substr(1, answer.size() - 2) :
+		tmp += answer.substr(0, answer.size() - 1);
+	if (*answer.rbegin() != '.') tmp += *answer.rbegin();
+
+	return step_4(tmp);
+}
+
+string step_2(string answer) {
+	string tmp = "";
+	for (int i = 0; i < answer.size() - 1; i++) {
+		if (!(answer[i] == '.' && answer[i + 1] == '.'))
+			tmp += answer[i];
+	}
+	if (*answer.rbegin() != '.') tmp += *answer.rbegin();
+
+	return step_3(tmp);
+}
+
+string step_1(string answer) {
+	string tmp = "";
+	for (int i = 0; i < answer.size(); i++) {
+		if (answer[i] == '-' || answer[i] == '_' || answer[i] == '.' || isalnum(answer[i])) {
+			if (isupper(answer[i]))
+				tmp += tolower(answer[i]);
+			else
+				tmp += answer[i];
+		}
+	}
+	return step_2(tmp);
+}
+
 string solution(string new_id) {
-	return step_4(step_3(step_2(step_1(new_id))));
+	return step_1(new_id);
 }
