87ce38da8a541bfec7c770a790b62b977ee0678a
suengwone <52214766+suengwone@users.noreply.github.com>
2021-09-09 00:34:41 +0900

    Create 2020_ Parenthesis_Conversion.cpp

---
+++ WEEK_10_KAKAO_CODING_TEST/LSW/2020_ Parenthesis_Conversion.cpp
@@ -0,0 +1,71 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+string isReverse(string p);
+string split(string p);
+
+string isReverse(string p) {
+    string tmp = p.substr(1, p.size() - 2);
+    for (int i = 0; i < tmp.size(); i++) {
+        if (tmp[i] == '(')
+            tmp[i] = ')';
+        else if (tmp[i] == ')')
+            tmp[i] = '(';
+    }
+
+    
+    return tmp;
+}
+
+string split(string p) {
+    if (p.empty()) return p;
+
+    int count_f = 0, count_r = 0;
+
+    int count = 0;
+
+    string answer;
+
+    bool isForward;
+
+    for (int i = 0; i < p.size(); i++) {
+        if (p[i] == '(') {
+            count++;
+        }
+        else {
+            count--;
+        }
+
+
+        if (count == 0 && isForward) {
+            answer += p.substr(0, i + 1);
+            answer += split(p.substr(i + 1, p.size() - (i + 1)));
+            return answer;
+        }
+        else if (count == 0 && !isForward) {
+            answer += "(" + split(p.substr(i + 1, p.size() - (i + 1))) + ")";
+            answer += isReverse(p.substr(0, i + 1));
+            return answer;
+        }
+
+        if (count < 0)
+            isForward = false;
+        else
+            isForward = true;
+    }
+}
+
+string solution(string p) {
+    return split(p);
+}
+
+int main(void) {
+    vector<string> p = { "(()())()" ,"))()((" ,"()))((()" };
+
+    for (string i : p) {
+        cout << solution(i) << '\n';
+    }
+}
