9628d84ece223a7e452c30d99547e5cbdd4d8cbe
suengwone <52214766+suengwone@users.noreply.github.com>
2021-09-18 17:20:09 +0900

    Create 9012.cpp

---
+++ WEEK_11_STRING_PROCESSING/LSW/9012.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	int T;
+	cin >> T;
+
+	string PS;
+	int count;
+
+	while (T--) {
+		count = 0;
+		cin >> PS;
+		for (int i = 0; i < PS.size(); i++) {
+			PS[i] == '(' ? count++ : count--;
+			if (count < 0) break;
+		}
+
+		count == 0 ? cout << "YES\n" : cout << "NO\n";
+	}
+}
